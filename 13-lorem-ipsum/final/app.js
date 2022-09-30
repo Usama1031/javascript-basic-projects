b05dbd5f9f2f0c46d609371f0d6f6a6967671341
Usama1031 <68164963+Usama1031@users.noreply.github.com>
2022-09-30 10:11:58 +0500

    Update app.js

--- 13-lorem-ipsum/final/app.js
+++ 13-lorem-ipsum/final/app.js
@@ -23,7 +23,7 @@ form.addEventListener("submit", function (e) {
   const value = parseInt(amount.value);
   const random = Math.floor(Math.random() * text.length);
 
-  if (isNaN(value) || value < 0 || value > 9) {
+  if (isNaN(value) || value <= 0 || value > 9) {
     result.innerHTML = `<p class="result">${text[random]}</p>`;
   } else {
     let tempText = text.slice(0, value);
